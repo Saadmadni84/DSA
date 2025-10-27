e2ace2359688d6903f73eb04a738753768d29634
Saad Madni <saadmadni84@gmail.com>
2025-10-27 22:58:32 +0530

    Time: 0 ms (100.00%) | Memory: 45.6 MB (32.21%) - LeetSync

--- 783-search-in-a-binary-search-tree/search-in-a-binary-search-tree.java
+++ 783-search-in-a-binary-search-tree/search-in-a-binary-search-tree.java
@@ -15,14 +15,13 @@
  */
 class Solution {
     public TreeNode searchBST(TreeNode root, int val) {
-        while(root!=null && root.val!=val){
-            if(val<root.val){
-                root=root.left;
-            }
-            else{
-                root=root.right;
-            }
+        if (root == null || root.val == val) {
+            return root;
         }
-        return root;
+        if (val < root.val) {
+            return searchBST(root.left, val);
+        } else {
+            return searchBST(root.right, val);
+        }
     }
 }
