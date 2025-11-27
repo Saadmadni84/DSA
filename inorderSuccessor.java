da58b239073e249d8e39dfb503b2a8e24cbc416d
Saad Madni <saadmadni84@gmail.com>
2025-11-27 23:13:42 +0530

    "26"

---
+++ inorderSuccessor.java
@@ -0,0 +1,14 @@
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+    TreeNode(int x) { val = x; }
+}
+public class InorderSuccessor {
+    public static void main(String[] args) {
+        Treenode 
+        while
+
+       
+    }
+}
