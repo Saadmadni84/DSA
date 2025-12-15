0c9b0b77939c094a1c8d1531f576d90db8cdb767
Saad Madni <saadmadni84@gmail.com>
2025-12-15 23:19:19 +0530

    Time: 0 ms (100.00%) | Memory: 41.7 MB (66.92%) - LeetSync

--- 1013-fibonacci-number/fibonacci-number.java
+++ 1013-fibonacci-number/fibonacci-number.java
@@ -3,12 +3,13 @@ class Solution {
       if(n<=1){
         return n;
       } 
-      int [] mem=new int[n+1];
-      mem[0]=0;
-      mem[1]=1;
+      int sl=0, l=1, ans=-1;
       for(int i=2;i<=n;i++) {
-        mem[i]=mem[i-1]+mem[i-2];
+        ans=sl+l;
+        sl=l;
+        l=ans;
+        
       }
-      return mem[n];
+      return ans;
     }
 }
