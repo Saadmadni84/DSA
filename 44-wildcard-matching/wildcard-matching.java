0bcd56c8fef255570522b6a0921f4246854e6bb2
Saad Madni <saadmadni84@gmail.com>
2025-12-29 23:36:41 +0530

    Time: 49 ms (34.30%) | Memory: 46.8 MB (74.74%) - LeetSync

--- 44-wildcard-matching/wildcard-matching.java
+++ 44-wildcard-matching/wildcard-matching.java
@@ -2,38 +2,32 @@ class Solution {
      public boolean isMatch(String s, String p) {
        int n=s.length();
        int m=p.length();
-       boolean[][] dp=new boolean[n+1][m+1];
-       dp[0][0]=true;
-       for(int i=1;i<=n;i++){
-        dp[i][0]=false;
-       }
-       for (int j=1;j<=m;j++){
-        if(p.charAt(j-1)=='*'){
-            dp[0][j]=dp[0][j-1];
-        }
-        dp[0][j]=false;
-       }
+       boolean [] prev=new boolean[m+1];
+        boolean [] curr=new boolean[m+1];
+        prev[0]=true;
+          curr[0] = false;
        for (int j = 1; j <= m; j++) {
             if (p.charAt(j - 1) == '*') {
-                dp[0][j] = dp[0][j - 1];  // '*' can match empty sequence
+                prev[j] = prev[j - 1];  
             } else {
-                dp[0][j] = false;
+                prev[j] = false;
             }
         }
-
        for(int i=1;i<=n;i++){
+      
          for (int j=1;j<=m;j++){
             if(s.charAt(i-1)==p.charAt(j-1) || p.charAt(j-1)=='?'){
-                dp[i][j]=dp[i-1][j-1];
+                curr[j]=prev[j-1];
             }
             else if(p.charAt(j-1)=='*'){
-                dp[i][j]=dp[i-1][j] || dp[i][j-1];
+                curr[j]=prev[j] || curr[j-1];
             }
             else{
-                dp[i][j]=false;
+                curr[j]=false;
             }
          }
+         prev=curr.clone();
        }
-       return dp[n][m];
+       return prev[m];
     }
 }
