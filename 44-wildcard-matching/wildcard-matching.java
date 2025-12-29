e3cf84b88cb9d3d4b3ca031a797407a5a3fe0097
Saad Madni <saadmadni84@gmail.com>
2025-12-29 22:22:28 +0530

    Time: 52 ms (31.25%) | Memory: 47.7 MB (38.79%) - LeetSync

--- 44-wildcard-matching/wildcard-matching.java
+++ 44-wildcard-matching/wildcard-matching.java
@@ -1,34 +1,39 @@
 class Solution {
-    private int helper(String s, String p,int i,int j,int [][] dp){
-        if(s.length()==i && p.length()==j){
-            return 1;
+     public boolean isMatch(String s, String p) {
+       int n=s.length();
+       int m=p.length();
+       boolean[][] dp=new boolean[n+1][m+1];
+       dp[0][0]=true;
+       for(int i=1;i<=n;i++){
+        dp[i][0]=false;
+       }
+       for (int j=1;j<=m;j++){
+        if(p.charAt(j-1)=='*'){
+            dp[0][j]=dp[0][j-1];
         }
-        if(p.length()==j){
-            return 0;
-        }
-        
-        if(dp[i][j]!=-1){
-            return dp[i][j];
-        }
-        if(s.length()==i){
-            if(p.charAt(j)=='*'){
-               return dp[i][j]=helper(s,p,i,j+1,dp);
+        dp[0][j]=false;
+       }
+       for (int j = 1; j <= m; j++) {
+            if (p.charAt(j - 1) == '*') {
+                dp[0][j] = dp[0][j - 1];  // '*' can match empty sequence
+            } else {
+                dp[0][j] = false;
             }
-            return dp[i][j]=0;
         }
-        if(s.charAt(i)==p.charAt(j) || p.charAt(j)=='?'){
-           return dp[i][j]= helper(s,p,i+1,j+1,dp);
-        }
-        if(p.charAt(j)=='*'){
-           return dp[i][j]= (helper(s,p,i,j+1,dp)==1|| helper(s,p,i+1,j,dp)==1)?1:0;
-        }
-        return dp[i][j]=0;
-    }
-    public boolean isMatch(String s, String p) {
-     int [][] dp=new int[s.length()+1][p.length()+1] ;
-     for(int []row:dp){
-        Arrays.fill(row,-1);
-     }  
-     return helper(s, p, 0, 0, dp) == 1;
+
+       for(int i=1;i<=n;i++){
+         for (int j=1;j<=m;j++){
+            if(s.charAt(i-1)==p.charAt(j-1) || p.charAt(j-1)=='?'){
+                dp[i][j]=dp[i-1][j-1];
+            }
+            else if(p.charAt(j-1)=='*'){
+                dp[i][j]=dp[i-1][j] || dp[i][j-1];
+            }
+            else{
+                dp[i][j]=false;
+            }
+         }
+       }
+       return dp[n][m];
     }
 }
