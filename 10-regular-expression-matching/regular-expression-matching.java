ba693db76b008d072c0a0d11666128ca4d820f1a
Saad Madni <saadmadni84@gmail.com>
2026-01-01 05:36:00 +0530

    Time: 1 ms (99.87%) | Memory: 44 MB (13.55%) - LeetSync

--- 10-regular-expression-matching/regular-expression-matching.java
+++ 10-regular-expression-matching/regular-expression-matching.java
@@ -1,31 +1,43 @@
 class Solution {
-    boolean helper(String s, String p,int i,int j){
+    boolean helper(String s, String p,int i,int j,Boolean [][] dp){
         if(i<0 && j<0){
             return true;
         }
         if(j<0){
             return false;
         }
+        boolean ans;
         if(i<0){
             if (p.charAt(j) == '*') {
-                return helper(s, p, i, j - 2);
+                ans=helper(s, p, i, j - 2,dp);
             }
-            return false;
-        }
-        if(s.charAt(i)==p.charAt(j) || p.charAt(j)=='.'){
-           return  helper(s,p,i-1,j-1);
-        }
-        if(p.charAt(j)=='*'){
-           return  helper(s,p,i,j-2) || ((s.charAt(i)==p.charAt(j-1) || p.charAt(j-1)=='.')
-           && helper(s,p,i-1,j));
+            else{
+            ans=false;
             }
+            dp[i + 1][j + 1] = ans;
+            return ans;
+        }
+        if(dp[i+1][j+1]!=null){
+            return dp[i+1][j+1];
+        }
+        if (s.charAt(i) == p.charAt(j) || p.charAt(j) == '.') {
+            ans = helper(s, p, i - 1, j - 1, dp);
+        } else if (p.charAt(j) == '*') {
+            ans = helper(s, p, i, j - 2, dp) ||
+                  ((s.charAt(i) == p.charAt(j - 1) || p.charAt(j - 1) == '.') &&
+                   helper(s, p, i - 1, j, dp));
+        } else {
+            ans = false;
+        }
 
-        return false;
+        dp[i + 1][j + 1] = ans;
+        return ans;
     }
 
     public boolean isMatch(String s, String p) {
         int i=s.length();
         int j=p.length();
-     return helper(s, p,i-1,j-1);   
+        Boolean [][] dp =new Boolean [i+1][j+1];
+     return helper(s, p,i-1,j-1,dp);   
     }
 }
