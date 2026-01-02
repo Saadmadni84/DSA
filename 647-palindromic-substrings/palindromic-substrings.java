bab85e1d1e26dcc7f6208e3ce3229f8c2c129814
Saad Madni <saadmadni84@gmail.com>
2026-01-03 01:49:00 +0530

    Time: 19 ms (28.48%) | Memory: 46.8 MB (18.53%) - LeetSync

--- 647-palindromic-substrings/palindromic-substrings.java
+++ 647-palindromic-substrings/palindromic-substrings.java
@@ -1,38 +1,21 @@
 class Solution {
-
-    private boolean helper(String s, int i, int j, int[][] dp) {
-        if (i > j) return false;
-        if (i == j) {
-            dp[i][j] = 1;
-            return true;
-        }
-        if (dp[i][j] != -1) {
-            return dp[i][j] == 1;
-        }
-        helper(s, i + 1, j, dp);
-        helper(s, i, j - 1, dp);
-        if (s.charAt(i) == s.charAt(j) &&
-            (j == i + 1 || helper(s, i + 1, j - 1, dp))) {
-            dp[i][j] = 1;
-            return true;
-        }
-        dp[i][j] = 0;
-        return false;
-    }
     public int countSubstrings(String s) {
         int n = s.length();
-        int[][] dp = new int[n][n];
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n; j++) {
-                dp[i][j] = -1;
-            }
-        }
-        helper(s, 0, n - 1, dp);
+        boolean [][] dp = new boolean[n][n];
         int res = 0;
         for (int l = 1; l <=n; l++) {
             for (int i = 0; i <=n-l; i++) {
                 int j=i+l-1;
-                if (dp[i][j] == 1) {
+                if(i==j){
+                    dp[i][j]=true;
+                }
+                else if(j==i+1){
+                   dp[i][j]=(s.charAt(i)==s.charAt(j));
+                }
+                else if(s.charAt(i)==s.charAt(j)){
+                    dp[i][j]=dp[i+1][j-1];
+                }
+                if(dp[i][j]){
                     res++;
                 }
             }
