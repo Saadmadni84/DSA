a67f22d55b1697774580c94bce9768526bd396ae
Saad Madni <saadmadni84@gmail.com>
2026-01-24 22:09:48 +0530

    Time: 2 ms (89.79%) | Memory: 48.1 MB (12.85%) - LeetSync

--- 283-move-zeroes/move-zeroes.java
+++ 283-move-zeroes/move-zeroes.java
@@ -1,28 +1,17 @@
 class Solution {
     public void moveZeroes(int[] a) {
-        int i=0;
-        int j=1;
-        while(j<a.length){
-            if(a[i]==0 && a[j]!=0){
-                swap(a,i,j);
-                i++;
-                j++;
-            }
-            else if(a[i]==0 && a[j]==0){
-               j++;
+    int k = 0; 
 
-            }
-            else{
-                i++;
-                j++;
-            }
+    for (int i = 0; i < a.length; i++) {
+        if (a[i] != 0) {
+            a[k] = a[i];
+            k++;
         }
-        
-        
     }
-    private void swap(int [] a,int i,int j){
-            int t=a[i];
-            a[i]=a[j];
-            a[j]=t;
-        }
+
+    while (k < a.length) {
+        a[k] = 0;
+        k++;
+    }
+}
 }
