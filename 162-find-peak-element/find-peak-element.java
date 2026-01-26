a057b5f54f302a91b40cc75abcf4a6051f65d23f
Saad Madni <saadmadni84@gmail.com>
2026-01-27 00:34:12 +0530

    Time: 0 ms (100.00%) | Memory: 44.5 MB (11.59%) - LeetSync

---
+++ 162-find-peak-element/find-peak-element.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int findPeakElement(int[] nums) {
+     return peak(nums);
+    }
+    
+    private int peak(int[] a) {
+        int l = 0, r = a.length - 1;
+        while (l < r) {
+            int m = l + (r - l) / 2;
+           
+            if (a[m] < a[m + 1]) {
+                l = m + 1;
+            } else {
+                
+                r = m;
+            }
+        }
+        return l; 
+}
+}
