49d5ee4d9cc7c3da82981b24b20bbd61712ec65a
Saad Madni <saadmadni84@gmail.com>
2026-02-04 02:32:16 +0530

    Implement isIsomorphic method in Solution class

---
+++ 205-isomorphic-strings/map.java
@@ -0,0 +1,29 @@
+class Solution {
+    public boolean isIsomorphic(String s, String t) {
+       Map<Character, Character> mapS = new HashMap<>();  
+       Map<Character, Character> mapT = new HashMap<>();  
+        
+        for(int i = 0; i < s.length(); i++) {
+            char sChar = s.charAt(i);
+            char tChar = t.charAt(i);
+
+            if(mapS.containsKey(sChar)) {
+                if(mapS.get(sChar) != tChar) {
+                    return false;
+                }
+            } 
+            else {
+                mapS.put(sChar, tChar);
+            }
+            if(mapT.containsKey(tChar)) {
+                if(mapT.get(tChar) != sChar) {
+                    return false;
+                }
+            } else {
+                mapT.put(tChar, sChar);
+            }
+        }
+        
+        return true;
+    }
+}
