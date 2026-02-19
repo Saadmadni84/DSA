bf587565874cb200959e9b8eb890c249fe4e5728
Saad Madni <saadmadni84@gmail.com>
2026-02-20 02:29:37 +0530

    Implement sortList method to sort a linked list

---
+++ 148-sort-list/b2.java
@@ -0,0 +1,27 @@
+class Solution {
+    public ListNode sortList(ListNode head) {
+        if (head == null || head.next == null) return head;
+        int count = 0;
+        ListNode curr = head;
+        while (curr != null) {
+            count++;
+            curr = curr.next;
+        }
+        int[] vals = new int[count];
+        curr = head;
+        for (int i = 0; i < count; i++) {
+            vals[i] = curr.val;
+            curr = curr.next;
+        }
+
+        Arrays.sort(vals);
+
+        curr = head;
+        for (int i = 0; i < count; i++) {
+            curr.val = vals[i];
+            curr = curr.next;
+        }
+
+        return head;
+    }
+}
