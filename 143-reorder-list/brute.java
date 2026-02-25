29352e509c1f8a98feabd3f1b032f5a38f48c542
Saad Madni <saadmadni84@gmail.com>
2026-02-26 04:32:43 +0530

    Implement reorderList method for linked list

---
+++ 143-reorder-list/brute.java
@@ -0,0 +1,27 @@
+class Solution {
+    public void reorderList(ListNode head) {
+        if(head==null){
+            return;
+        }
+     ListNode curr=head;
+     List<Integer> ls=new ArrayList<>();
+     while(curr!=null){
+       ls.add(curr.val);
+       curr=curr.next;
+     }
+     int i=0,j=ls.size()-1;
+     curr = head;
+     while(i<=j){
+        if(i==j){
+             curr.val=ls.get(i);
+             break;
+        }
+        curr.val=ls.get(i);
+        curr=curr.next;
+        curr.val=ls.get(j);
+        curr=curr.next;
+        i++;
+        j--;
+     }  
+    }
+}
