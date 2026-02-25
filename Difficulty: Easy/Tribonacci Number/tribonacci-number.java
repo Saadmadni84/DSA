03ed7b41a8e4ad6b5582cdb9a1ddfe26888e8801
Saad Madni <saadmadni84@gmail.com>
2026-02-25 17:26:21 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Tribonacci Number/tribonacci-number.java
@@ -0,0 +1,31 @@
+
+class Solution {
+    public static int nthTribonacci(int n) {
+        if(n==0){
+            return 0;
+        }
+        if(n==1 || n==2){
+            return 1;
+        }
+        int [] dp=new int[n+1];
+
+        for(int i=0;i<=n;i++){
+            dp[i]=-1;
+        }
+        return helper(dp,n);
+    }
+    private static int helper(int [] dp,int n){
+         if(n==0){
+            return 0;
+        }
+        if(n==1 || n==2){
+            return 1;
+        }
+        if(dp[n]!=-1){
+            return dp[n];
+        }
+        dp[n]=helper(dp,n-1)+helper(dp,n-2)+helper(dp,n-3);
+
+        return dp[n];
+    }
+}
