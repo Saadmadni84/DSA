028c54575b2b67052d0c02406b77c2011c8982fd
Saad Madni <saadmadni84@gmail.com>
2026-02-28 02:06:25 +0530

    Time: 0 ms (100.00%) | Memory: 43 MB (36.42%) - LeetSync

---
+++ 198-house-robber/house-robber.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int rob(int[] nums) {
+        int [] dp=new int[nums.length+1];
+        Arrays.fill(dp,-1);
+       return helper(nums,0,dp);
+    }
+    private int helper(int[] nums, int i,int [] dp) {
+        if (i >=nums.length) return 0;
+        if(dp[i]!=-1){
+            return dp[i];
+        }
+        int pick = nums[i] + helper(nums, i+2,dp);
+        int dontPick = helper(nums, i +1,dp);
+       dp[i]= Math.max(pick, dontPick);
+       return dp[i];
+    }
+}
