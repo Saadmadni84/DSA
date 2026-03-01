8c8a1f7e5ad2cc3567c9288473e42c3d41773993
Saad Madni <saadmadni84@gmail.com>
2026-03-02 00:12:07 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Subsets/subsets.java
@@ -0,0 +1,19 @@
+class Solution {
+    public ArrayList<ArrayList<Integer>> subsets(int nums[]) {
+        ArrayList<ArrayList<Integer>> result = new ArrayList<>();
+        generateSubsets(nums, 0, new ArrayList<>(), result);
+        return result;
+    }
+
+    private void generateSubsets(int[] nums, int i, ArrayList<Integer> current, ArrayList<ArrayList<Integer>> result) {
+        if (i == nums.length) {
+            result.add(new ArrayList<>(current));
+            return;
+        }
+
+        current.add(nums[i]);
+        generateSubsets(nums, i + 1, current, result);
+        current.remove(current.size() - 1);
+        generateSubsets(nums, i + 1, current, result);
+    }
+}
