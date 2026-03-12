a2b7f053e0e5bc52f149873478f02739b6e15e97
Saad Madni <saadmadni84@gmail.com>
2026-03-13 01:22:31 +0530

    Create different.java

---
+++ 145-binary-tree-postorder-traversal/different.java
@@ -0,0 +1,21 @@
+class Solution {
+    public List<Integer> postorderTraversal(TreeNode root) {
+     LinkedList<Integer> res = new LinkedList<>();
+        if (root == null) return res;
+
+        Stack<TreeNode> stack = new Stack<>();
+        stack.push(root);
+
+        while (!stack.isEmpty()) {
+            TreeNode curr = stack.pop();
+            res.addFirst(curr.val);
+            if (curr.left != null) {
+                stack.push(curr.left);
+            }
+            if (curr.right != null) {
+                stack.push(curr.right);
+            }
+        }
+        return res;
+    }
+}
