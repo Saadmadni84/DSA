fe326faed5844fe141017498cbe0fb9fa0fbb4d4
Saad Madni <saadmadni84@gmail.com>
2026-05-24 16:12:31 +0530

    Implement DFS to check path existence in graph

---
+++ 2121-find-if-path-exists-in-graph/adjlist(dfs).java
@@ -0,0 +1,33 @@
+class Solution {
+
+    private boolean dfs(Map<Integer, List<Integer>> graph, int src, int dest, boolean[] vis, int n) {
+        if (src == dest) return true;
+
+        vis[src] = true;
+
+        for (int neighbor : graph.get(src)) {
+            if (!vis[neighbor]) {
+                if (dfs(graph, neighbor, dest, vis, n))
+                    return true;
+            }
+        }
+
+        return false;
+    }
+
+    public boolean validPath(int n, int[][] edges, int source, int destination) {
+
+        Map<Integer, List<Integer>> graph = new HashMap<>();
+
+        for (int[] edge : edges) {
+            int u = edge[0], v = edge[1];
+
+            graph.computeIfAbsent(u, value -> new ArrayList<>()).add(v);
+            graph.computeIfAbsent(v, value -> new ArrayList<>()).add(u);
+        }
+
+        boolean[] vis = new boolean[n];
+
+        return dfs(graph, source, destination, vis, n);
+    }
+}
