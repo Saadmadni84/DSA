f633dbb435ef3bb4ecba19a2116a666486c52a8b
Saad Madni <saadmadni84@gmail.com>
2026-06-01 16:12:25 +0530

    Time: 4 ms (93.15%) | Memory: 47.2 MB (46.35%) - LeetSync

--- 207-course-schedule/course-schedule.java
+++ 207-course-schedule/course-schedule.java
@@ -1,45 +1,37 @@
 class Solution {
-    public boolean canFinish(int numCourses, int[][] prerequisites) {
-
-        List<List<Integer>> graph = new ArrayList<>();
-
-        for(int i = 0; i < numCourses; i++) {
-            graph.add(new ArrayList<>());
+    public boolean canFinish(int n, int[][] ps) {
+        List<List<Integer>> g=new ArrayList<>();
+        for(int i=0;i<n;i++){
+           g.add(new ArrayList<>());
         }
-
-        int[] indegree = new int[numCourses];
-
-        for(int[] pre : prerequisites) {
-            int course = pre[0];
-            int prereq = pre[1];
-
-            graph.get(prereq).add(course);
-            indegree[course]++;
+        for(int [] p:ps){
+            g.get(p[0]).add(p[1]);//directed so only one side
         }
-
-        Queue<Integer> q = new LinkedList<>();
-
-        for(int i = 0; i < numCourses; i++) {
-            if(indegree[i] == 0) {
-                q.offer(i);
-            }
-        }
-
-        int count = 0;
-
-        while(!q.isEmpty()) {
-            int node = q.poll();
-            count++;
-
-            for(int neigh : graph.get(node)) {
-                indegree[neigh]--;
-
-                if(indegree[neigh] == 0) {
-                    q.offer(neigh);
+        boolean [] visiting =new boolean[n];
+        boolean [] visited =new boolean[n];
+        for(int i=0;i<n;i++){
+            if(!visited[i]){
+                if(!dfs(i,n,g,visiting,visited)){
+                    return false;
                 }
             }
         }
-
-        return count == numCourses;
+        return true;
+    }
+    private boolean dfs(int curr,int n,List<List<Integer>> g,boolean [] visiting,boolean [] visited){
+        visiting[curr]=true;
+        for(int ne:g.get(curr)){
+            if(visiting[ne]==true){ //cycle detecte
+                return false;
+            }
+            if(!visited[ne]){
+                if(!dfs(ne,n,g,visiting,visited)){
+                    return false;
+                }
+            }
+        }
+        visiting[curr]=false;
+        visited[curr]=true;
+        return true;
     }
 }
