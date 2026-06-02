8252f5dce6f8aed327c6b4de5094b87f19b96ae3
Saad Madni <saadmadni84@gmail.com>
2026-06-02 18:04:28 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Course Schedule I/course-schedule-i.java
@@ -0,0 +1,37 @@
+class Solution {
+    public boolean canFinish(int n, int[][] ps) {
+    List<List<Integer>> g=new ArrayList<>();
+    for(int i=0;i<n;i++){
+        g.add(new ArrayList<>());
+    }
+    for(int [] p:ps){
+        g.get(p[0]).add(p[1]);
+    }
+    boolean [] visiting =new boolean[n];
+     boolean [] visited =new boolean[n] ;
+     for(int i=0;i<n;i++){
+         if(!visited[i]){
+             if(!dfs(i,n,g,visiting,visited)) return false;
+         }
+     }
+     return true;
+    }
+     public boolean dfs(int curr,int n,List<List<Integer>> g,boolean [] visiting,boolean [] visited){
+     visiting[curr]=true;
+     for(int ne:g.get(curr)){
+         if(visiting[ne]){
+             return false;
+         }
+          if(!visited[ne]){
+         if(!dfs(ne,n,g,visiting,visited)){
+             return false;
+         }
+          }
+     }
+     
+     visiting[curr]=false;
+     visited[curr]=true;
+     return true;
+    
+    }
+}
