00bb1c346c107454aa4353975922683cfda739f8
Saad Madni <saadmadni84@gmail.com>
2026-06-09 00:26:10 +0530

    Time: 13 ms (46.03%) | Memory: 49.7 MB (28.26%) - LeetSync

--- 744-network-delay-time/network-delay-time.java
+++ 744-network-delay-time/network-delay-time.java
@@ -1,23 +1,49 @@
 class Solution {
     public int networkDelayTime(int[][] times, int n, int k) {
-        int [] dist=new int[n+1];
-        Arrays.fill(dist,Integer.MAX_VALUE);
-        dist[k]=0;
-        for(int i=1;i<n;i++){ //n-1 times bellman ford
-           for(int [] t:times){
-                int u=t[0],v=t[1],w=t[2];
-                if(dist[u]+w<dist[v] && dist[u]!=Integer.MAX_VALUE){
-                    dist[v]=dist[u]+w; //relaxation
+        int[][] dist = new int[n][n];
+        // Initialize with infinity
+        for (int i = 0; i < n; i++) {
+            Arrays.fill(dist[i], Integer.MAX_VALUE);
+        }
+        // Direct edges
+        for (int[] time : times) {
+            int u = time[0];
+            int v = time[1];
+            int w = time[2];
+            dist[u - 1][v - 1] = w; // convert to 0-based indexing
+        }
+        // Diagonal = 0
+        for (int i = 0; i < n; i++) {
+            dist[i][i] = 0;
+        }
+        // Floyd-Warshall
+        for (int via = 0; via < n; via++) {
+            for (int i = 0; i < n; i++) {
+                for (int j = 0; j < n; j++) {
+
+                    if (dist[i][via] != Integer.MAX_VALUE &&
+                        dist[via][j] != Integer.MAX_VALUE) {
+
+                        dist[i][j] = Math.min(
+                            dist[i][j],
+                            dist[i][via] + dist[via][j]
+                        );
+                    }
                 }
             }
         }
-        int res=-1;
-        for(int i=1;i<=n;i++){
-            res=Math.max(res,dist[i]);
+
+        int res = Integer.MIN_VALUE;
+
+        // Distances from source k
+        for (int i = 1; i <=n; i++) {
+            if (dist[k - 1][i-1] == Integer.MAX_VALUE) {
+                return -1; // unreachable node
+            }
+
+            res = Math.max(res, dist[k - 1][i-1]);
         }
-        if(res==Integer.MAX_VALUE){
-            return -1;
-        }
+
         return res;
     }
 }
