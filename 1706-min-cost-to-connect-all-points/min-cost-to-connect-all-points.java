bf56caf0337e5d3f996f1459ecf35c48b5ec2c06
Saad Madni <saadmadni84@gmail.com>
2026-06-11 01:44:44 +0530

    Time: 328 ms (43.33%) | Memory: 98.8 MB (23.84%) - LeetSync

--- 1706-min-cost-to-connect-all-points/min-cost-to-connect-all-points.java
+++ 1706-min-cost-to-connect-all-points/min-cost-to-connect-all-points.java
@@ -1,34 +1,88 @@
 class Solution {
-    public int minCostConnectPoints(int[][] points) {
-        int n=points.length;
-        int [][] g=new int[n][n]; //forming graph
-        //connecting all nodes
-        for(int i=0;i<n;i++){
-           int [] p=points[i];
-           for(int j=0;j<n;j++){
-            if(j==i) continue; //same number to same
-            g[i][j]=Math.abs(p[0]-points[j][0])+Math.abs(p[1]-points[j][1]);
-           }
-        }
-        PriorityQueue<int []> pq=new PriorityQueue<>((a,b)->Integer.compare(a[0],b[0]));
-        pq.offer(new int[] {0,0});
-        int [] vis=new int[n];
-        int res=0;
-        while(!pq.isEmpty()){
-            int [] curr=pq.poll();
-            int cost=curr[0];
-            int node=curr[1];
-            if(vis[node]==1){
-                continue;
+// prims solution is on github and in copy visit that its easy
+    class DisjointSet {
+        int[] parent;
+        int[] rank;
+
+        public DisjointSet(int n) {
+            parent = new int[n];
+            rank = new int[n];
+
+            for (int i = 0; i < n; i++) {
+                parent[i] = i;
             }
-            vis[node]=1;
-            res=res+cost;
-            for(int i=0;i<n;i++){
-                if(vis[i]==0){
-                    pq.offer(new int [] {g[node][i],i});
+        }
+
+        public int find(int x) {
+            if (parent[x] != x) {
+                parent[x] = find(parent[x]); // Path Compression
+            }
+            return parent[x];
+        }
+
+        public boolean union(int x, int y) {
+            int rootX = find(x);
+            int rootY = find(y);
+
+            if (rootX == rootY) {
+                return false;
+            }
+
+            // Union by Rank
+            if (rank[rootX] < rank[rootY]) {
+                parent[rootX] = rootY;
+            } else if (rank[rootX] > rank[rootY]) {
+                parent[rootY] = rootX;
+            } else {
+                parent[rootY] = rootX;
+                rank[rootX]++;
+            }
+
+            return true;
+        }
+    }
+
+    public int minCostConnectPoints(int[][] points) {
+
+        int n = points.length;
+        List<int[]> edges = new ArrayList<>();
+
+        // Create all possible edges
+        for (int i = 0; i < n; i++) {
+            for (int j = i + 1; j < n; j++) {
+
+                int cost = Math.abs(points[i][0] - points[j][0]) +
+                           Math.abs(points[i][1] - points[j][1]);
+
+                edges.add(new int[]{cost, i, j});
+            }
+        }
+
+        // Sort edges by cost
+        Collections.sort(edges, (a, b) -> a[0] - b[0]);
+
+        DisjointSet ds = new DisjointSet(n);
+
+        int totalCost = 0;
+        int edgesUsed = 0;
+
+        // Kruskal's Algorithm
+        for (int[] edge : edges) {
+
+            int cost = edge[0];
+            int u = edge[1];
+            int v = edge[2];
+
+            if (ds.union(u, v)) {
+                totalCost += cost;
+                edgesUsed++;
+
+                if (edgesUsed == n - 1) {
+                    break;
                 }
             }
         }
-        return res;
+
+        return totalCost;
     }
 }
