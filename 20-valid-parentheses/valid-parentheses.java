8bbb3396f00d50879257a4738a67fc6f54fb7a74
Saad Madni <saadmadni84@gmail.com>
2026-07-03 18:55:42 +0530

    Time: 3 ms (87.34%) | Memory: 43.1 MB (72.68%) - LeetSync

--- 20-valid-parentheses/valid-parentheses.java
+++ 20-valid-parentheses/valid-parentheses.java
@@ -1,31 +1,20 @@
-import java.util.Stack;
 
 class Solution {
     public boolean isValid(String s) {
         Stack<Character> stack = new Stack<>();
 
         for (char c : s.toCharArray()) {
-            switch (c) {
-                case '(':
-                case '{':
-                case '[':
-                    stack.push(c);
-                    break;
-                case ')':
-                    if (stack.isEmpty() || stack.pop() != '(') {
-                        return false;
-                    }
-                    break;
-                case '}':
-                    if (stack.isEmpty() || stack.pop() != '{') {
-                        return false;
-                    }
-                    break;
-                case ']':
-                    if (stack.isEmpty() || stack.pop() != '[') {
-                        return false;
-                    }
-                    break;
+            if(c=='('){
+                stack.push(')');
+            }
+            else if(c=='['){
+                stack.push(']');
+            }
+            else if(c=='{'){
+                stack.push('}');
+            }
+            else if(stack.isEmpty() || stack.pop()!=c){
+                return false;
             }
         }
         
