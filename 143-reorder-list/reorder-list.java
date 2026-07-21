43fbf1811a4a0201239330d7fd233b1da1aeeb36
Saad Madni <saadmadni84@gmail.com>
2026-07-22 02:36:12 +0530

    Time: 2 ms (87.10%) | Memory: 49.4 MB (35.83%) - LeetSync

--- 143-reorder-list/reorder-list.java
+++ 143-reorder-list/reorder-list.java
@@ -10,32 +10,41 @@
  */
 class Solution {
     public void reorderList(ListNode head) {
-      ListNode curr=head;
-      int c=0;
-      while(curr!=null){
-           c++;
-           curr=curr.next;
-      }
-      curr=head;
-      int [] arr=new int[c];
-      int f=0;
-      while(curr!=null){
-        arr[f]=curr.val;
-        curr=curr.next;
-        f++;
-      }
-      int i=0;
-      int j=c-1;
-      while(i<=j){
-         head.val=arr[i];
-         head=head.next;
-         i++;
-         if(i<=j){
-            head.val=arr[j];
-            head=head.next;
-              j--;
-         }
+        if(head==null || head.next==null){
+            return ;
+        }
+        ListNode slow=head;
+        ListNode fast=head;
+        while(fast!=null && fast.next!=null){
+            slow=slow.next;
+            fast=fast.next.next;
+        }
+        ListNode prev=null;
+        ListNode curr=slow.next;
+        slow.next = null;
+        while(curr!=null){
+            ListNode temp=curr.next;
+            curr.next=prev;
+            prev=curr;
+            curr=temp;
+        }
+        ListNode first=head;
+        ListNode second =prev;
+
+        while(second!=null){
+            ListNode temp1=first.next;
+            ListNode temp2=second.next;
+
+            first.next=second;
+            second.next=temp1;
+
+            first=temp1;
+            second=temp2;
+
+
+        }
+
        
-      }
+      
     }
 }
