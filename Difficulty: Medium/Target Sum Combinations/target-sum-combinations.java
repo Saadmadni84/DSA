b3e079d85483f3d593daab76d565302a2fdb6575
Saad Madni <saadmadni84@gmail.com>
2026-07-29 22:43:01 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Target Sum Combinations/target-sum-combinations.java
+++ Difficulty: Medium/Target Sum Combinations/target-sum-combinations.java
@@ -1,22 +1,22 @@
 class Solution {
-    public ArrayList<ArrayList<Integer>> targetSumComb(int[] candidates, int target) {
-    ArrayList<ArrayList<Integer>> ls=new ArrayList<>();
-        int sum=0;
-        helper(candidates,target,0,ls,new ArrayList<>(),sum);
-        return ls;
+    public ArrayList<ArrayList<Integer>> targetSumComb(int[] arr, int target) {
+      ArrayList<ArrayList<Integer>> ls=new ArrayList<>();
+      int sum=0;
+      helper(arr,0,new ArrayList<>(),target,sum,ls);
+      return ls; 
     }
-    private void helper(int [] c,int t,int i,ArrayList<ArrayList<Integer>> ls,ArrayList<Integer> curr,int sum){
-        if(sum==t){
+    private void helper(int [] arr,int i,ArrayList<Integer> curr,int target,int sum,ArrayList<ArrayList<Integer>> ls){
+        if(sum==target){
             ls.add(new ArrayList<>(curr));
             return;
         }
-        if (sum > t) {
+         if (sum > target) {
             return;
             }
 
-        for(int f=i;f<c.length;f++){
-            curr.add(c[f]);
-            helper(c,t,f,ls,curr,sum+c[f]);
+        for(int f=i;f<arr.length;f++){
+            curr.add(arr[f]);
+            helper(arr,f,curr,target,sum+arr[f],ls);
             curr.remove(curr.size()-1);
         }
     }
