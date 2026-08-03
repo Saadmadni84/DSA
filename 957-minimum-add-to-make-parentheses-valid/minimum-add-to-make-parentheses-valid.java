41775b705d0accfe10ecdb48e634b9625419754a
Saad Madni <saadmadni84@gmail.com>
2026-08-04 00:43:30 +0530

    Time: 1 ms (67.19%) | Memory: 42.8 MB (77.79%) - LeetSync

---
+++ 957-minimum-add-to-make-parentheses-valid/minimum-add-to-make-parentheses-valid.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int minAddToMakeValid(String s) {
+       int l = 0;
+        int r = 0; 
+        
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            if (c == '(') {
+                l++;
+            } else {
+                if (l > 0) {
+                    l--; 
+                } else {
+                    r++; 
+                }
+            }
+        }
+        
+        return l + r;
+    }
+}
