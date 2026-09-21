a0bb9cac289e395ca01df37c54a6717f6456e7a3
Saad Madni <saadmadni84@gmail.com>
2026-09-22 01:52:31 +0530

    Time: 1 ms (98.00%) | Memory: 47.2 MB (19.66%) - LeetSync

---
+++ 57-insert-interval/insert-interval.java
@@ -0,0 +1,26 @@
+import java.util.ArrayList;
+import java.util.List;
+
+class Solution {
+    public int[][] insert(int[][] arr, int[] ni) {
+        List<int[]> ls = new ArrayList<>();
+        int i = 0;
+        int n = arr.length;
+        while (i < n && arr[i][1] < ni[0]) {
+            ls.add(arr[i]);
+            i++;
+        }
+        while (i < n && arr[i][0] <= ni[1]) {
+            ni[0] = Math.min(ni[0], arr[i][0]);
+            ni[1] = Math.max(ni[1], arr[i][1]);
+            i++;
+        }
+        ls.add(ni);
+        while (i < n) {
+            ls.add(arr[i]);
+            i++;
+        }
+
+        return ls.toArray(new int[ls.size()][]);
+    }
+}
